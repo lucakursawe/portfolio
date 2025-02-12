6a4589ae41c949eb9070dfcb42e84a96ae3cd07f
Luca Kursawe <30121038+lucakursawe@users.noreply.github.com>
2025-02-12 16:59:12 +0100

    added analytics and speed insights via html

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-    const icon = themeToggle.querySelector("i");
+  const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
+  const icon = themeToggle.querySelector("i");
 
-    // Check if the user has a saved preference
-    if (localStorage.getItem("theme") === "light") {
-        body.classList.add("light-mode");
-        icon.classList.replace("fa-moon", "fa-sun");
+  // Check if the user has a saved preference
+  if (localStorage.getItem("theme") === "light") {
+    body.classList.add("light-mode");
+    icon.classList.replace("fa-moon", "fa-sun");
+  }
+
+  themeToggle.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+
+    // Update icon
+    if (body.classList.contains("light-mode")) {
+      icon.classList.replace("fa-moon", "fa-sun");
+      localStorage.setItem("theme", "light");
+    } else {
+      icon.classList.replace("fa-sun", "fa-moon");
+      localStorage.setItem("theme", "dark");
     }
-
-    themeToggle.addEventListener("click", () => {
-        body.classList.toggle("light-mode");
-
-        // Update icon
-        if (body.classList.contains("light-mode")) {
-            icon.classList.replace("fa-moon", "fa-sun");
-            localStorage.setItem("theme", "light");
-        } else {
-            icon.classList.replace("fa-sun", "fa-moon");
-            localStorage.setItem("theme", "dark");
-        }
-    });
+  });
 });
